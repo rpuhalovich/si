345f059b008ebdce021559411f43b529dae040d4
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-09-30 20:35:40 +1000

    Formatting

--- src/main.c
+++ src/main.c
@@ -11,20 +11,18 @@ int main()
     bool showMessageBox = false;
     GuiLoadStyleDark();
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
 
         ClearBackground(GetColor(GuiGetStyle(DEFAULT, BACKGROUND_COLOR)));
 
-        if (GuiButton((Rectangle){ 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
+        if (GuiButton((Rectangle) { 24, 24, 120, 30 }, "#191#Show Message")) showMessageBox = true;
 
-        if (showMessageBox)
-        {
-            int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 },
-                "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
+        if (showMessageBox) {
+            int result = GuiMessageBox((Rectangle){ 85, 70, 250, 100 }, "#191#Message Box", "Hi! This is a message!", "Nice;Cool");
 
-            if (result >= 0) showMessageBox = false;
+            if (result >= 0)
+                showMessageBox = false;
         }
 
         EndDrawing();
