dffafad58935075d70d9f3fc271f8f4c8d965a7a
Ryan Puhalovich <reanpuhalovich@gmail.com>
2024-11-17 21:36:34 +1100

    Cleanup draw.h

--- src/draw.h
+++ src/draw.h
@@ -8,18 +8,12 @@
 #include "appmath.h"
 #include "state.h"
 
-void drawStringbg(const char* str, i32 strLen, AppFont* font, Color color, Color bg, Vector2 pos);
+void drawDebugRec(Rectangle r);
 
-void drawLine(Line* l, AppFont* font, Color color, Vector2 pos);
+void drawDebugPoint(Vector2 p);
 
-void drawString(const char* str, i32 strLen, AppFont* font, Color color, Vector2 pos);
-
-void drawBuffer(Rectangle bounds, Buffer* b, AppFont* font, Color textColor, Color cursorColor);
+void drawThing(const Thing* t);
 
 void draw(AppState* state);
-
-void drawApp(AppState* state);
-
-void drawComponentCanvas(AppState* state);
 
 #endif // DRAW_H
